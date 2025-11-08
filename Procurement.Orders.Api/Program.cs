298a2b8c78a51adba77e9bc3374938de80dd553c
Vahid Saadat <vst121@gmail.com>
2025-11-08 20:17:00 +0100

    Make operational Orders.Aoi and Notifications.Api

--- Procurement.Orders.Api/Program.cs
+++ Procurement.Orders.Api/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // Disabled for development to allow HTTP
 
 var summaries = new[]
 {
